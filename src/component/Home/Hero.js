15cf9dd60ff440c98306fc31e2e37fb0b9aba7d5
Suriya94454 <suriya94454@gmail.com>
2023-12-02 16:00:43 +0530

    hero section changed

--- src/component/Home/Hero.js
+++ src/component/Home/Hero.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+
 
 function Hero() {
   return (
-    <h1>hi</h1>
-  )
+    <div className="Hero">
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-lg-6 col-12">
+            <h1 style={{ fontSize: "50px" }}>
+              <b>Helping Each Other Can Make World Better</b>
+            </h1>
+            <p>
+              We Seek out world changers and difference makers around the
+              globe,and equip them to fulfill their unique purpose.
+            </p>
+            <button className="herobtn">Know About Us</button>
+          </div>
+          <div className="col-lg-6 col-12"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
