afd38f42fca380f902d4deb86c70bbde04d345ec
Dinesh-M-India <chandru08112000@gmail.com>
2023-12-03 17:46:43 +0530

    CHanged

--- src/component/Home/OurEvents.js
+++ src/component/Home/OurEvents.js
@@ -5,8 +5,10 @@ function OurEvents() {
     <section>
       <div className="container py-5">
         <span className="d-flex align-items-center justify-content-center">
-            <span className="ourEvents">Our Events &nbsp;&nbsp;</span>
-            <span style={{ width: '80%' }}><hr /></span>
+          <span className="ourEvents">Our Events &nbsp;&nbsp;</span>
+          <span style={{ width: "80%" }}>
+            <hr />
+          </span>
         </span>
       </div>
     </section>
