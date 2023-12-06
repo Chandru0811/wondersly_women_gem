ef53258398a26bc01989dc1dd0a007aa51c00be3
Suriya94454 <suriya94454@gmail.com>
2023-12-06 16:43:36 +0530

    Testimonial error sloved

--- src/component/Home/Aboutus.js
+++ src/component/Home/Aboutus.js
@@ -1,20 +1,30 @@
 import React from "react";
+import aboutUs from "../../asset/aboutUs.png";
 
 function Aboutus() {
   return (
     <div className="Aboutus">
-      <div className="container">
+      <div className="container" style={{ paddingTop: "100px"}}>
         <div className="row">
-          <div className="col-lg-6 col-md-6 col-12"></div>
-          <div className="col-lg-6 col-md-6 col-12">
-            <div className="row">
-              <div className="col-2">
-                <p>About Us</p>
-              </div>
-              <div className="col-10">
-                <hr />
-              </div>
-            </div>
+          <div className="col-lg-7 col-md-6 col-12">
+            <img src={aboutUs} className="img-fluid"></img>
+          </div>
+          <div className="col-lg-5 col-md-6 col-12" style={{ marginTop: "150px"}}>
+            <span className="d-flex align-items-center ">
+              <span className="aboutName">
+                <b>About Us</b> &nbsp;&nbsp;
+              </span>
+              <span>
+                <hr className="aboutHr" />
+              </span>
+            </span>
+            <h1>Your Support is Really Powerful.</h1>
+            <p>
+              The secret to happiness lies in helping others. Never
+              underestimate the difference YOU can make in the lives of the
+              poor, the abused and the helpless.
+            </p>
+            <button className="my-button">Read More</button>
           </div>
         </div>
       </div>
