cfccf1524968e078f8cb0c9c8306bdfec6f4ebea
Suriya94454 <suriya94454@gmail.com>
2023-12-08 15:38:07 +0530

    Hero Section Changed

--- src/component/Home/Hero.js
+++ src/component/Home/Hero.js
@@ -7,17 +7,16 @@ function Hero() {
     <div className="Hero">
       <div className="container py-5">
         <div className="row">
-          <div className="col-lg-6 col-12">
-            <h1 style={{ fontSize: "60px" }}>
-              <b>Helping Each Other Can Make World Better</b>
+          <div className="col-lg-6 col-12 mt-5">
+            <h1 style={{ fontSize: "38px" }}>
+              <b>BEHIND EVERY SUCCESSFUL WOMAN IS A TRIBE OF OTHER SUCCESSFUL WOMEN, WHO HAVE HER BACK</b>
             </h1>
             <p className="my-4">
-              We Seek out world changers and difference makers around the
-              globe,and equip them to fulfill their unique purpose.
+            Women belong in all places where decisions are being made. It shouldn't be that women are the exception. And one day she discovered that she was fierce, and strong, and full of fire, and that not even she could hold herself back because her passion burned brighter than her fears.
             </p>
             <button className="herobtn">Know About Us</button>
           </div>
-          <div className="col-lg-6 col-12">
+          <div className="col-lg-6 col-12 mt-3">
             <img src={HeroImg} className="img-fluid" alt="Hero_Img"></img>
           </div>
         </div>
