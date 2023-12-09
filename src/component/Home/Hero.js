2014a8e93e6d0815222ea2fc215c071fc3ef3112
Dinesh-M-India <chandru08112000@gmail.com>
2023-12-09 11:02:05 +0530

    Test

--- src/component/Home/Hero.js
+++ src/component/Home/Hero.js
@@ -5,18 +5,18 @@ import HeroImg from "../../asset/Hero_Img.png";
 function Hero() {
   return (
     <div className="Hero">
-      <div className="container py-5">
+      <div className="container py-3">
         <div className="row">
-          <div className="col-lg-6 col-12 mt-5">
-            <h1 style={{ fontSize: "38px" }}>
+          <div className="col-lg-6 col-md-12 col-12 d-flex flex-column justify-content-center">
+            <h1 className="HeroHeading">
               <b>BEHIND EVERY SUCCESSFUL WOMAN IS A TRIBE OF OTHER SUCCESSFUL WOMEN, WHO HAVE HER BACK</b>
             </h1>
-            <p className="my-4">
+            <p className="mt-2 mb-4">
             Women belong in all places where decisions are being made. It shouldn't be that women are the exception. And one day she discovered that she was fierce, and strong, and full of fire, and that not even she could hold herself back because her passion burned brighter than her fears.
             </p>
             <button className="herobtn">Know About Us</button>
           </div>
-          <div className="col-lg-6 col-12 mt-3">
+          <div className="col-lg-6 col-md-12 col-12 d-flex flex-column justify-content-center align-items-center">
             <img src={HeroImg} className="img-fluid" alt="Hero_Img"></img>
           </div>
         </div>
