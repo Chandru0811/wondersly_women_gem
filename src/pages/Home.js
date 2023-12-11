0e6eef2584fe55afcc3bd7c4a74c0a1367768bb0
Suriya94454 <suriya94454@gmail.com>
2023-12-11 18:16:10 +0530

    HomeAdded

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import Hero from "../component/Home/Hero";
 import Help from "../component/Home/Help";
 import NonProfitable from "../component/Home/NonProfitable";
-import WWGAbout from "../component/About/WWGAbout";
+// import WWGAbout from "../component/About/WWGAbout";
 import Supporters from "../component/Home/Supporters";
 import OurEvents from "../component/Home/OurEvents";
 import Testimonial from "../component/About/Testimonial";
@@ -12,7 +12,7 @@ import Aboutus from "../component/Home/Aboutus";
 import WelcomeCharity from "../component/Home/WelcomeCharity";
 import Awards from "../component/Home/Awards";
 import OurVolunteers from "../component/Home/OurVolunteers";
-import LatestCauses from "../component/Home/LatestCauses";
+// import LatestCauses from "../component/Home/LatestCauses";
 import Contribute from "../component/Home/Contribute";
 
 function Home() {
