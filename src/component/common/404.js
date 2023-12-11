118cde20e5333ab392bb9fde939cc18fb2dae09f
Suriya94454 <suriya94454@gmail.com>
2023-12-11 19:12:45 +0530

    404 page added

--- src/component/common/404.js
+++ src/component/common/404.js
@@ -1,7 +1,28 @@
 import React from "react";
+import errorpagr from "../../asset/404 error page.png";
+import { Link } from "react-router-dom";
 
 function Page404() {
-  return <div>404 Error page not found</div>;
+  return (
+    <section className="errorPage">
+      <div className="container" style={{ marginTop: "200px", minHeight: "70vh" }}>
+        <div className="row">
+          <div className="d-flex flex-column align-items-center justify-content-center">
+          <h1>Sorry, Page not found!</h1>
+            <Link
+            to="/">
+            <button type="button" className="gotoBtn btn btn-danger mb-5">Go to Homepage</button>
+            </Link>
+            <img
+              className="errorImg img-fluid mt-2"
+              src={errorpagr}
+              alt="errorpage"
+            ></img>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Page404;
