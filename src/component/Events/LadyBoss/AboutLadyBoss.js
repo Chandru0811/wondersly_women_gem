c3cb13bbabfe5bbc3fc513728a44eae12f43c80e
Suriya94454 <suriya94454@gmail.com>
2023-12-12 18:41:21 +0530

    Working on LadyBoss BC

--- src/component/Events/LadyBoss/AboutLadyBoss.js
+++ src/component/Events/LadyBoss/AboutLadyBoss.js
@@ -1,14 +1,47 @@
 import React from "react";
+import LadyBossLogo from "../../../asset/LadyBossLogo.png";
 
 function AboutLadyBoss() {
   return (
-    <section className="aboutladyBoss">
+    <section className="aboutladyBoss mb-3">
       <div className="container-fluid">
         <div className="row">
           <p className="ourBoardTeam mt-3">Events</p>
-          <h1 className="ourBoardTeamText">LadyBoss Business Connect</h1>
-          <div className="aboutladybossContent" style={{ minHeight: "56vh" }}>
-            <h1>hi</h1>
+          <h1 className="ourBoardTeamText mb-5">LadyBoss Business Connect</h1>
+          <div className="col-lg-2 col-md-1 col-1 d-flex flex-column align-items-center">
+            <img
+              src={LadyBossLogo}
+              className="img-fluid half-image-top"
+              alt="ladybossLogo"
+              style={{ maxWidth: "100%", maxHeight: "100%" }}
+            />
+          </div>
+          <div className="col-lg-10 col-md-11 col-11">
+            <div className="aboutladybossContent row">
+              <div className="col-lg-1 col-md-1 col-1"></div>
+              <div className="col-lg-10 col-md-10 col-10">
+                <div
+                  className=" d-flex flex-column justify-content-center align-items-center"
+                  style={{ minHeight: "56vh" }}
+                >
+                  <h1 className="ladybossHeading">
+                    ABOUT LADYBOSS BUSINESS CONNECT
+                  </h1>
+                  <h5 className="ladybossQuotes">
+                    “Turning your passion into profits”
+                  </h5>
+                  <p className="turningContent text-center mt-3">
+                    Turning passion into profit is the idea of making money by
+                    doing something you deeply enjoy. Its about creating ideas
+                    out of your interest or hobbies. By turning your passion
+                    into a business, you can do something you love everyday
+                    while earning the income. It's a way to make work feel like
+                    play.
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-1 col-md-1 col-1"></div>
+            </div>
           </div>
         </div>
       </div>
