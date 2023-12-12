431a003f6b9d28f684b98e861d04778b7ab88b27
Dinesh-M-India <chandru08112000@gmail.com>
2023-12-12 18:39:49 +0530

    Childrens Page Added

--- src/pages/Events/Children.js
+++ src/pages/Events/Children.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import ChildHero from '../../component/Events/Children/ChildHero'
+import ChildBody from '../../component/Events/Children/ChildBody'
 
 function Children() {
   return (
     <div style={{ marginTop: "118px" }}>
-      Children
+      <ChildHero />
+      <ChildBody />
     </div>
   )
 }
