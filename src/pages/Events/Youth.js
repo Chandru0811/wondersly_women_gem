637f7dcd11639bef2f962436264b4ecc8f3020bd
Dinesh-M-India <chandru08112000@gmail.com>
2023-12-12 17:25:53 +0530

    Youth Page added

--- src/pages/Events/Youth.js
+++ src/pages/Events/Youth.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import YouthHero from '../../component/Events/Youth/YouthHero'
+import YouthBody from '../../component/Events/Youth/YouthBody'
 
 function Youth() {
   return (
     <div style={{ marginTop: "118px" }}>
       <YouthHero />
+      <YouthBody />
     </div>
   )
 }
