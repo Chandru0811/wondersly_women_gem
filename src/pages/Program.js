df00dbeb4c2265da6405780a0bf8ec5b9a4dcd96
Chandru0811 <chandru08112000@gmail.com>
2024-01-28 15:34:49 +0530

    28/1/24 updated committee

--- src/pages/Program.js
+++ src/pages/Program.js
@@ -3,11 +3,13 @@ import Services from "../component/About/Services";
 import Testimonial from "../component/About/Testimonial";
 import Solution from "../component/Home/Solution";
 import Count from "../component/Home/Count";
+import UpcommingBody from "../component/Events/Upcomming/UpcommingBody";
 
 function Program() {
   return (
     <div style={{ marginTop: "118px" }}>
       <Services />
+      <UpcommingBody />
       <Testimonial />
       <Solution />
       <Count /> 
