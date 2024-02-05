d7e90daa997263c0d7ab07784b9ad5d8367f602c
Suriya94454 <suriya94454@gmail.com>
2024-02-05 16:45:56 +0530

    changed heading font size for mobile view

--- src/component/Home/Hero.js
+++ src/component/Home/Hero.js
@@ -1,52 +1,43 @@
 import React from "react";
 import HerStoryMission from "../../asset/HerStoryMission.png";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
-const handleClick = () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-};
+// const handleClick = () => {
+//   window.scrollTo({ top: 0, behavior: "smooth" });
+// };
 
 function Hero() {
   return (
     <div className="Hero">
-      <div className="container py-3">
-        <div className="row">
-          <div className="col-lg-1"></div>
-          <div
-            data-aos="fade-up"
-            className="col-lg-10 col-md-10 col-12 d-flex flex-column align-items-center justify-content-center"
-          >
-            <h1 className="HeroHeading">
-              <b>
-                BEHIND EVERY SUCCESSFUL WOMAN IS A TRIBE OF OTHER SUCCESSFUL
-                WOMEN, WHO HAVE HER BACK
-              </b>
-            </h1>
-            <p className="mt-2 text-center">
+      <div className="container my-5">
+        <div data-aos="fade-up" className="row">
+          <div className="col-lg-1" style={{ visibility: "hidden" }}>
+            <hr className="aboutHr"></hr>
+          </div>
+          <div className="col-lg-10 col-md-10 col-12 pt-1">
+            <span className="aboutName" style={{ visibility: "hidden" }}>
+              HER STORY
+            </span>
+            <h2 className="heroheading mb-3">
+              BEHIND EVERY SUCCESSFUL WOMAN IS A TRIBE OF OTHER SUCCESSFUL
+              WOMEN, WHO HAVE HER BACK
+            </h2>
+            <p className="historyMissionText mb-3">
               At WWGEM, we firmly believe in the strength and resilience of
               single mothers and women, empowering them to rebuild their lives
               as independent individuals.
             </p>
-            <div
-              data-aos="fade-down"
-              className="d-flex flex-column justify-content-center align-items-center"
-            >
-              <img
-                src={HerStoryMission}
-                className="img-fluid"
-                alt="Hero_Img"
-              ></img>
-            </div>
-            <p className="mt-2">
+            <img
+              src={HerStoryMission}
+              alt="HerStoryMission"
+              className="img-fluid mb-3"
+              style={{ width: "100%" }}
+            />
+            <p className="historyMissionText mb-5">
               We have inspired over 5000 women across various countries and the
               ASEAN region through self-development programs, holistic support,
               education enhancement, entrepreneurship, and sports activities.
             </p>
-            <div className="d-flex justify-content-center">
-              <Link to="/about" onClick={handleClick}>
-                <button className="herobtn">Learn more</button>
-              </Link>
-            </div>
           </div>
           <div className="col-lg-1"></div>
         </div>
